55002b1092f6b9b4e4a18e7e70c401395fae43bf
Danny Brian <danny@brians.org>
2018-05-03 11:04:46 -0600

    enhancements for Columbus

--- nofluff.js
+++ nofluff.js
@@ -5,16 +5,19 @@
         constructor() {
             super();
             this._parseRegex = new RegExp(/{{([^{}]+)}}/, 'g');
+            this._data = {};
         }
         
+        // added stubs for connected
         connectedCallback () {
-            console.log('connected');
+            console.log(`connected: ${this.nodeName}`);
         }
         
-        attributeChangedCallback () {
-            
+        // ... and attribute changed
+        attributeChangedCallback (a, b, c) {
         }
         
+        // use some data 
         set data (data) {
             this._data = data;
         }
@@ -23,128 +26,144 @@
             return this._data;
         }
 
+        // iterate the attributes for an element, and interpolate
         iterateAttributes (node, values) {
             node = (node === undefined) ? this : node;
-            
             values = (values === undefined) ? this._data : values;
             
-            for (let i = 0; i < node.attributes.length; i++) {
-                var avalue = node.attributes[i].value;
+            for (var i = 0; i < node.attributes.length; i++) {
+                let avalue = node.attributes[i].value;
                 if (avalue === undefined) { continue; }
-                var interped = this.interpolateText(avalue, values);
+                let interped = this.interpolateText(avalue, values);
                 node.attributes[i].value = interped;
             }
         }
     
+        iterateNodes (nodes, values) {
+            nodes = (nodes === undefined) ? this.childNodes : nodes;
+            values = (values === undefined) ? this._data : values;
+            
+            for (var i = 0; i < nodes.length; i++) {
+                if (nodes[i].nodeType === Node.ELEMENT_NODE) {
+                    this.iterateAttributes(nodes[i], values);    
+                }
+                
+                /*if (nodes[i].nodeName.toUpperCase().startsWith("NO-FLUFF")) {
+                    continue;
+                }*/
+                
+                //console.log(`${nodes[i].nodeName}   ${nodes[i].nodeValue}`);
+                if (nodes[i].nodeType === Node.TEXT_NODE) {
+                    nodes[i].nodeValue = this.interpolateText(nodes[i].nodeValue, values);
+                }
+                this.iterateNodes(nodes[i].childNodes, values);    
+            }
+            
+        }
+        
+        // moved interpolate to bottom
         interpolateText (str, values) {
-            return str.replace(this._parseRegex, function(a, b) { // match, capture
+            let replaced = str.replace(this._parseRegex, function(a, b) { // match, capture
                 let r = values[b];
                 return typeof r === 'string' || typeof r === 'number' ? r : a;
             });
-        }
-        
-        iterateNodes(nodes, values) {
-            nodes = (nodes === undefined) ? this.childNodes : nodes;
-            
-            values = (values === undefined) ? this._data : values;
-            
-            for (let i = 0; i < nodes.length; i++) {
-                
-                if (nodes[i].nodeName.toUpperCase().startsWith('NO-FLUFF')) {
-                    console.log("SKIPPING NO-FLUFF");
-                    return;
-                }
-                
-                if (nodes[i].nodeType === Node.ELEMENT_NODE) {
-                    this.iterateAttributes(nodes[i], values);
-                }
-                
-                var nvalue = nodes[i].nodeValue;
-                
-                if (nodes[i].nodeType === Node.TEXT_NODE) {
-                        let interped = this.interpolateText(nvalue, values);
-                        nodes[i].nodeValue = interped;
-                }
-                
-                this.iterateNodes(nodes[i].childNodes, values);
-            }
+            return replaced;
         }
     }
     
     class NoFluffRepeat extends NoFluff {
-        constructor() {
+        
+        constructor (hooks) {
+            // { dataFetched: function() { }, repeaterFinished: function () {} }
             super();
+            
         }
+    
         connectedCallback () {
             this._sourceUrl = this.getAttribute('data-source');
-            this._template = this.querySelector('template'); 
-            // call fetch and set timer to continually poll
-            this.fetchData();
+            let self = this;
+            
+            self._template = document.querySelector('template');
+                
+            self.fetchData();
+            
         }
         
         fetchData () {
-            var self = this;
+            let self = this;
             fetch(this._sourceUrl).then(function(response) {
-                response.json().then(function(data) { // stream, JSON parsed async
-                    self.renderRepeat(data);
-                });
+               response.json().then(function(data) {
+                   self.renderRepeat(data);
+               });
             });
         }
         
         renderRepeat (data) {
-            var self = this;
             for (let i = 0; i < data.length; i++) {
-                // just use IDs
-                let already = document.getElementById(data.id);
-                if (already) {
-                    this.iterateNodes(already.childNodes, data[i]);
+                let already = this.querySelector('[id="' + data[i].id + '"]');
+                if (already !== null) {
+                    this.iterateNodes(already);
                 }
                 else
                 {
-                    let newrow = document.importNode(this._template.content, true);
-                    let appended = this.appendChild(newrow);
+                    let cloned = document.importNode(this._template.content, true);
+                    
+                    this.appendChild(cloned);
                     this.iterateNodes(this.childNodes, data[i]);
                 }
             }
-            // setTimeout(function() { self.fetchData(); }, 5000);
-
         }
     }
     
+    class NoFluffSpeaker extends NoFluff {
+        constructor () {
+            super();
+        }
+        
+        connectedCallback () {
+            let speakerid = location.hash.substr(1);
+            if (speakerid === '') { return; }
+            if (speakerid === this.id) {
+                this.classList.remove('hide');
+            }
+            else
+            {
+                this.classList.add('hide');
+            }
+        }
+    }
+    
+    // register the custom element
     customElements.define('no-fluff', NoFluff);
     customElements.define('no-fluff-repeat', NoFluffRepeat);
+    customElements.define('no-fluff-speaker', NoFluffSpeaker);
     
-    window.addEventListener("hashchange", hashChanged, false);
+    document.querySelector('no-fluff').iterateNodes(undefined,
+        { title: "Rich Web ", description: "Here are some speakers:" });
+    
+    window.addEventListener('hashchange', hashChanged, false);
     function hashChanged (e) {
-        let ourid = location.hash.substr(1);
-        let el = document.getElementById(ourid);
-        if (el !== null) {
-            for (let i = 0; i < el.parentNode.childNodes.length; i++) {
-                if (el.parentNode.childNodes[i].nodeType !== Node.ELEMENT_NODE) {
-                    continue;
-                }
-                console.log(el.parentNode.childNodes[i]);
-                if (el.parentNode.childNodes[i] === el) {
-                    el.parentNode.childNodes[i].classList.remove('hide');
+        let hash = location.hash;
+        let ourid = hash.substr(1);
+        let speaker = document.getElementById(ourid);
+        if (speaker === null) { return; }
+        for (let i = 0; i < speaker.parentNode.childNodes.length; i++) {
+            let currentNode = speaker.parentNode.childNodes[i];
+            if (currentNode.nodeType === Node.ELEMENT_NODE) {
+                if (currentNode === speaker) {
+                    currentNode.classList.remove('hide');
                 }
                 else
                 {
-                    el.parentNode.childNodes[i].classList.add('hide');
+                    currentNode.classList.add('hide');
                 }
             }
         }
     }
     
+    setTimeout(function() {
+        hashChanged();
+    },1000);
+
+    
 }());
-
-// set the data manually on the no-fluff element
-var nofluff = document.querySelector('no-fluff');
-nofluff.data = { title: "Speakers", description: "Here are some of the speakers." };
-nofluff.iterateNodes();
-
-// enhancements:
-// fade in each after added and image loaded
-// click the element to change location.hash
-// poll with a ticker to get updates to the JSON (needs to cache the old {{ }} )
-// performance enhacenements, 304, etc.
-
